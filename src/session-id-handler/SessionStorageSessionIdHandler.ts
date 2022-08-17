cc1faaa69a1f7f5ec0b4a19ac3b07c6c47864765
Christophe Grandpierre <c.grandpierre@smartesting.com>
2022-08-17 17:32:25 +0200

    remove useless console messages

--- src/session-id-handler/SessionStorageSessionIdHandler.ts
+++ src/session-id-handler/SessionStorageSessionIdHandler.ts
@@ -5,7 +5,6 @@ const GRAVITY_SESSION_STORAGE_KEY_SESSION_ID = 'gravity-session-id'
 export default class SessionStorageSessionIdHandler implements SessionIdHandler {
   get(): string {
     const sessionId = window.sessionStorage.getItem(GRAVITY_SESSION_STORAGE_KEY_SESSION_ID)
-    console.error('GET SESSION ID', sessionId)
     if (sessionId !== null) return sessionId
     throw new Error('Set session id before to use it')
   }
@@ -15,7 +14,6 @@ export default class SessionStorageSessionIdHandler implements SessionIdHandler 
   }
 
   set(sessionId: string): void {
-    console.error('SET SESSION ID', sessionId)
     window.sessionStorage.setItem(GRAVITY_SESSION_STORAGE_KEY_SESSION_ID, sessionId)
   }
 }
