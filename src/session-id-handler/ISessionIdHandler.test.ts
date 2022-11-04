821479a3b3f7cb4d6e73e6f98c2668555dfd2539
Rémi Federspiel <remi.federspiel@smartesting.com>
2022-11-04 11:24:44 +0100

    Fixing prettier warnings

--- src/session-id-handler/ISessionIdHandler.test.ts
+++ src/session-id-handler/ISessionIdHandler.test.ts
@@ -6,64 +6,58 @@ import SessionStorageSessionIdHandler from './SessionStorageSessionIdHandler'
 import CookieSessionIdHandler from './CookieSessionIdHandler'
 
 function iSessionIdHandlerContractTest(
-    implementationName: string,
-    makeSessionIdHandler: () => ISessionIdHandler,
-    cleanup: () => void,
+  implementationName: string,
+  makeSessionIdHandler: () => ISessionIdHandler,
+  cleanup: () => void,
 ) {
-    describe(implementationName, () => {
-        let sessionIdHandler: ISessionIdHandler
-        beforeEach(() => {
-            cleanup()
-            sessionIdHandler = makeSessionIdHandler()
-        })
-
-        describe('isSet', () => {
-            it('returns false when no session id has been set', () => {
-                assert.strictEqual(sessionIdHandler.isSet(), false)
-            })
-
-            it('returns true when a session id has been set', () => {
-                sessionIdHandler.set('abcd')
-                assert.strictEqual(sessionIdHandler.isSet(), true)
-            })
-        })
-
-        describe('get', () => {
-            it('throws an error when no session id has been set', () => {
-                assert.throws(() => sessionIdHandler.get(), new Error('Set session id before using it'))
-            })
-
-            it('returns the session when it has been set', () => {
-                const sessionId = 'abcd'
-                sessionIdHandler.set(sessionId)
-                assert.strictEqual(sessionIdHandler.get(), sessionId)
-            })
-        })
+  describe(implementationName, () => {
+    let sessionIdHandler: ISessionIdHandler
+    beforeEach(() => {
+      cleanup()
+      sessionIdHandler = makeSessionIdHandler()
     })
+
+    describe('isSet', () => {
+      it('returns false when no session id has been set', () => {
+        assert.strictEqual(sessionIdHandler.isSet(), false)
+      })
+
+      it('returns true when a session id has been set', () => {
+        sessionIdHandler.set('abcd')
+        assert.strictEqual(sessionIdHandler.isSet(), true)
+      })
+    })
+
+    describe('get', () => {
+      it('throws an error when no session id has been set', () => {
+        assert.throws(() => sessionIdHandler.get(), new Error('Set session id before using it'))
+      })
+
+      it('returns the session when it has been set', () => {
+        const sessionId = 'abcd'
+        sessionIdHandler.set(sessionId)
+        assert.strictEqual(sessionIdHandler.get(), sessionId)
+      })
+    })
+  })
 }
 
 iSessionIdHandlerContractTest(
-    'MemorySessionIdHandler',
-    () => new MemorySessionIdHandler(),
-    () => {
-    },
+  'MemorySessionIdHandler',
+  () => new MemorySessionIdHandler(),
+  () => {},
 )
 
 iSessionIdHandlerContractTest(
-    'SessionStorageSessionIdHandler',
-    () => new SessionStorageSessionIdHandler(),
-    () => sessionStorage.clear(),
+  'SessionStorageSessionIdHandler',
+  () => new SessionStorageSessionIdHandler(),
+  () => sessionStorage.clear(),
 )
 
-iSessionIdHandlerContractTest(
-    'CookieSessionIdHandler',
-    () => new CookieSessionIdHandler(),
-    clearCookies,
-)
+iSessionIdHandlerContractTest('CookieSessionIdHandler', () => new CookieSessionIdHandler(), clearCookies)
 
 function clearCookies() {
-    document.cookie.split(';').forEach(function (c) {
-        document.cookie = c.replace(/^ +/, '')
-            .replace(/=.*/, `=;expires=${new Date().toUTCString()};path=/`)
-    })
+  document.cookie.split(';').forEach(function (c) {
+    document.cookie = c.replace(/^ +/, '').replace(/=.*/, `=;expires=${new Date().toUTCString()};path=/`)
+  })
 }
