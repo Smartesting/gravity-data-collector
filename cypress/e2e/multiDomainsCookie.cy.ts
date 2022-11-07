32c03c8fdbb6e6367fe24d3423a54357d75f43a6
Rémi Federspiel <remi.federspiel@smartesting.com>
2022-11-07 12:02:23 +0100

    Fixing Cypress failing test

--- cypress/e2e/multiDomainsCookie.cy.ts
+++ cypress/e2e/multiDomainsCookie.cy.ts
@@ -6,41 +6,36 @@ describe('Handling sessions on multi-domain', () => {
   })
 
   it('uses the same session ID on a sub-domains', () => {
-    let mySiteSessionId: string = ''
-
-    cy.intercept(
-      {
-        method: 'POST',
-        url: `https://gravityserverstaging.herokuapp.com/api/tracking/${collectionAuthKey}/publish`,
-      },
-      {
-        statusCode: 200,
-        body: { error: null },
-      },
-    ).as('sendGravityRequest')
+    ;-cy
+      .intercept(
+        {
+          method: 'POST',
+          url: `https://gravityserverstaging.herokuapp.com/api/tracking/${collectionAuthKey}/publish`,
+        },
+        {
+          statusCode: 200,
+          body: { error: null },
+        },
+      )
+      .as('sendGravityRequest')
 
     cy.visit('http://my-site.com:3000/')
-    cy.get('button.activate-logger').click()
-
-    cy.get('a.simple-link').click()
     cy.wait('@sendGravityRequest').then((interception) => {
-      mySiteSessionId = interception.request.body[0].sessionId
-    })
+      const mySiteSessionId = interception.request.body[0].sessionId
 
-    cy.get('a.go-to-app').click()
-    cy.url().should('eq', 'http://app.my-site.com:3000/')
-    cy.get('button.activate-logger').click()
+      cy.get('a.go-to-app').click()
+      cy.wait('@sendGravityRequest').then((_) => {
+        cy.url().should('eq', 'http://app.my-site.com:3000/')
 
-    cy.get('a.simple-link').click()
-    cy.wait('@sendGravityRequest').then((interception) => {
-      const sessionId = interception.request.body[0].sessionId
-      expect(sessionId).to.eq(mySiteSessionId)
+        cy.get('a.simple-link').click()
+        cy.wait('@sendGravityRequest').then((interception2) => {
+          expect(interception2.request.body[0].sessionId).to.eq(mySiteSessionId)
+        })
+      })
     })
   })
 
   it('uses another sessions ID when navigating to another domain', () => {
-    let mySiteSessionId: string = ''
-
     cy.intercept(
       {
         method: 'POST',
@@ -53,26 +48,21 @@ describe('Handling sessions on multi-domain', () => {
     ).as('sendGravityRequest')
 
     cy.visit('http://my-site.com:3000/')
-    cy.get('button.activate-logger').click()
-    cy.get('a.simple-link').click()
     cy.wait('@sendGravityRequest').then((interception) => {
-      mySiteSessionId = interception.request.body[0].sessionId
-    })
+      const mySiteSessionId = interception.request.body[0].sessionId
 
-    cy.get('a.go-to-another-site').click()
-    cy.url().should('eq', 'http://auth.another-site.com:3000/')
+      cy.get('a.go-to-another-site').click()
+      cy.wait('@sendGravityRequest').then((_) => {
+        cy.url().should('eq', 'http://auth.another-site.com:3000/')
 
-    cy.get('button.activate-logger').click()
-    cy.get('a.simple-link').click()
-    cy.wait('@sendGravityRequest').then((interception) => {
-      const sessionId = interception.request.body[0].sessionId
-      expect(sessionId).not.to.eq(mySiteSessionId)
+        cy.wait('@sendGravityRequest').then((interception2) => {
+          expect(interception2.request.body[0].sessionId).not.to.eq(mySiteSessionId)
+        })
+      })
     })
   })
 
   it('finds back the original session ID when coming back to the source site', () => {
-    let mySiteSessionId: string = ''
-
     cy.intercept(
       {
         method: 'POST',
@@ -85,30 +75,27 @@ describe('Handling sessions on multi-domain', () => {
     ).as('sendGravityRequest')
 
     cy.visit('http://my-site.com:3000/')
-    cy.get('button.activate-logger').click()
-    cy.get('a.simple-link').click()
     cy.wait('@sendGravityRequest').then((interception) => {
-      mySiteSessionId = interception.request.body[0].sessionId
-    })
+      const mySiteSessionId = interception.request.body[0].sessionId
 
-    cy.get('a.go-to-another-site').click()
-    cy.url().should('eq', 'http://auth.another-site.com:3000/')
+      cy.get('a.go-to-another-site').click()
+      cy.wait('@sendGravityRequest').then((_) => {
+        cy.url().should('eq', 'http://auth.another-site.com:3000/')
 
-    cy.get('button.activate-logger').click()
-    cy.get('a.simple-link').click()
-    cy.wait('@sendGravityRequest').then((interception) => {
-      const sessionId = interception.request.body[0].sessionId
-      expect(sessionId).not.to.eq(mySiteSessionId)
-    })
+        cy.wait('@sendGravityRequest').then((interception2) => {
+          expect(interception2.request.body[0].sessionId).not.to.eq(mySiteSessionId)
 
-    cy.get('a.go-to-app').click()
-    cy.url().should('eq', 'http://app.my-site.com:3000/')
+          cy.get('a.go-to-app').click()
+          cy.wait('@sendGravityRequest').then((_) => {
+            cy.url().should('eq', 'http://app.my-site.com:3000/')
 
-    cy.get('button.activate-logger').click()
-    cy.get('a.simple-link').click()
-    cy.wait('@sendGravityRequest').then((interception) => {
-      const sessionId = interception.request.body[0].sessionId
-      expect(sessionId).to.eq(mySiteSessionId)
+            cy.get('a.simple-link').click()
+            cy.wait('@sendGravityRequest').then((interception3) => {
+              expect(interception3.request.body[0].sessionId).to.eq(mySiteSessionId)
+            })
+          })
+        })
+      })
     })
   })
 })
