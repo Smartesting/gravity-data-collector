74846ea830eb805da89e79de86c13c0372f1661e
Aymeric <aymeric.cretin@smartesting.com>
2022-12-06 12:01:14 +0100

    Call `encodeURIComponent` before to write a cookie Call `decodeURIComponent` after to read a cookie

--- src/utils/documentCookie.ts
+++ src/utils/documentCookie.ts
@@ -1,7 +1,7 @@
 import psl from 'psl'
 
 export function setCookie(key: string, value: string) {
-  let cookie = `${key}=${value}; path=/`
+  let cookie = `${key}=${encodeURIComponent(value)}; path=/`
   const parsedDomain = psl.parse(document.location.hostname)
   if (isParsedDomain(parsedDomain) && parsedDomain.domain !== null) {
     const domain: string = parsedDomain.domain
@@ -13,8 +13,7 @@ export function setCookie(key: string, value: string) {
 
 export function readCookie(key: string): string | undefined {
   const match = document.cookie.match(new RegExp(`(^| )${key}=([^;]+)`))
-
-  return match !== null ? match[2] : undefined
+  return match !== null ? decodeURIComponent(match[2]) : undefined
 }
 
 function isParsedDomain(toBeDetermined: any): toBeDetermined is psl.ParsedDomain {
