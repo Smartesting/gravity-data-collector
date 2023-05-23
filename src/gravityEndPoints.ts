78c7868eecccc5d78af0b762545d75ff85c59302
alevern <alexandre.vernotte@smartesting.com>
2023-05-23 16:05:40 +0200

    update server URL

--- src/gravityEndPoints.ts
+++ src/gravityEndPoints.ts
@@ -1,4 +1,4 @@
-export const GRAVITY_SERVER_ADDRESS = 'https://smartestinggravityserver.herokuapp.com'
+export const GRAVITY_SERVER_ADDRESS = 'https://gravityclient-canary.herokuapp.com'
 
 export function buildGravityTrackingPublishApiUrl(authKey: string, gravityServerUrl: string) {
   return `${trackingUrlStartPart(gravityServerUrl)}/${authKey}/publish`
