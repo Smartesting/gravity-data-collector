75acb76ee7b1194f52597c3996b0aa5ce2606d54
Vincent Pretre <vincent.pretre@gmail.com>
2023-06-05 13:49:52 +0200

    Revert previous change for textrea handling

--- src/utils/listeners.ts
+++ src/utils/listeners.ts
@@ -1,7 +1,7 @@
 import { HTMLInputWithValue } from '../types'
 
 const TAG_NAME_DISALLOWED_BY_KEY_LISTENERS: string[] = ['textarea']
-const INPUT_ALLOWED_BY_KEY_LISTENERS = ['radio', 'select', 'checkbox', 'button', 'textarea']
+const INPUT_ALLOWED_BY_KEY_LISTENERS = ['radio', 'select', 'checkbox', 'button']
 const KEYS_ALLOWED_BY_KEY_LISTENERS = ['tab', 'enter', 'numpadenter']
 
 export function isKeyAllowedByKeyListeners(keyCode: string): boolean {
@@ -12,11 +12,13 @@ export function isTargetAllowedByKeyListeners(target: EventTarget | null): boole
   if (target === null) return true
 
   const elementTarget = target as HTMLElement
+  const inputElementTarget = target as HTMLInputWithValue
+
   if (TAG_NAME_DISALLOWED_BY_KEY_LISTENERS.includes(elementTarget.tagName.toLowerCase())) return false
 
-  if (elementTarget.tagName.toLowerCase() === 'input' || elementTarget.tagName.toLowerCase() === 'textarea') {
-    const inputElementTarget = target as HTMLInputWithValue
+  if (elementTarget.tagName.toLowerCase() === 'input') {
     return INPUT_ALLOWED_BY_KEY_LISTENERS.includes(inputElementTarget.type)
   }
+
   return true
 }
