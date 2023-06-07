dff789f7d61f7f1e28ddd98662f5653c4fe6071f
Vincent Pretre <vincent.pretre@gmail.com>
2023-06-07 15:23:16 +0200

    Trigger a change event when typing in a text field

--- src/utils/listeners.ts
+++ src/utils/listeners.ts
@@ -3,6 +3,19 @@ import { HTMLInputWithValue } from '../types'
 const TAG_NAME_DISALLOWED_BY_KEY_LISTENERS: string[] = ['textarea']
 const INPUT_ALLOWED_BY_KEY_LISTENERS = ['radio', 'select', 'checkbox', 'button']
 const KEYS_ALLOWED_BY_KEY_LISTENERS = ['tab', 'enter', 'numpadenter']
+const NON_TEXT_KEYS = ['tab', 'enter', 'numpadenter', 'esc']
+
+export function recordChangeEvent(keyCode: string, target: EventTarget | null): boolean {
+  if (target === null) return false
+
+  const inputElementTarget = target as HTMLInputWithValue
+  if (INPUT_ALLOWED_BY_KEY_LISTENERS.includes(inputElementTarget.type)) return false
+
+  const elementTarget = target as HTMLElement
+  const isTextInput = (elementTarget.tagName.toLowerCase() === 'input' || elementTarget.tagName.toLowerCase() === 'textarea')
+
+  return isTextInput && !NON_TEXT_KEYS.includes(keyCode.toLowerCase())
+}
 
 export function isKeyAllowedByKeyListeners(keyCode: string): boolean {
   return keyCode !== undefined && KEYS_ALLOWED_BY_KEY_LISTENERS.includes(keyCode.toLowerCase())
