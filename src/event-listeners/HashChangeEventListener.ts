855a6fa2429cadf9a74ca5257e21f346265ed2d5
Vincent Pretre <vincent.pretre@gmail.com>
2023-11-23 10:58:10 +0100

    Run prettier

--- src/event-listeners/HashChangeEventListener.ts
+++ src/event-listeners/HashChangeEventListener.ts
@@ -3,7 +3,7 @@ import IUserActionHandler from '../user-action/IUserActionHandler'
 import { UserActionType } from '../types'
 
 export default class HashChangeEventListener extends TargetedEventListener {
-    constructor(userActionHandler: IUserActionHandler, window: Window, options: TargetEventListenerOptions = {}) {
-        super(userActionHandler, UserActionType.HashChange, window, options)
-    }
+  constructor(userActionHandler: IUserActionHandler, window: Window, options: TargetEventListenerOptions = {}) {
+    super(userActionHandler, UserActionType.HashChange, window, options)
+  }
 }
