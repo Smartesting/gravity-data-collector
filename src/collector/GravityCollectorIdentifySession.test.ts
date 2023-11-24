5d8f6d83c002dd553975e277743178ac3e0637f3
Christophe Grandpierre <c.grandpierre@smartesting.com>
2023-11-24 08:40:01 +0100

    refactor: remove sinon + use describe.each for all tests by contract

--- src/collector/GravityCollectorIdentifySession.test.ts
+++ src/collector/GravityCollectorIdentifySession.test.ts
@@ -1,42 +1,40 @@
 import { v4 as uuid } from 'uuid'
 import { SessionTraitValue } from '../types'
 import { afterEach, beforeEach, describe, expect, it, SpyInstance, vi } from 'vitest'
-import { CollectorInstaller, collectorInstaller } from './CollectorInstaller'
+import { collectorInstaller } from './CollectorInstaller'
 import { asyncNop, nop } from '../utils/nop'
 import SessionTraitHandler from '../session-trait/SessionTraitHandler'
 
-function contractTest(context: string, installer: () => CollectorInstaller) {
-  describe(`GravityCollector.identifySession() in ${context}`, () => {
-    let handleSessionTrait: SpyInstance<[traitName: string, traitValue: SessionTraitValue], Promise<void>>
-    let consoleWarn: SpyInstance<any[], void>
+describe.each([
+  { context: 'dry run mode (debug=true)', installer: () => collectorInstaller({ debug: true }) },
+  { context: 'live mode (debug=false)', installer: () => collectorInstaller({ debug: false, authKey: uuid() }) },
+])('GravityCollector.identifySession() in $context', ({ installer }) => {
+  let handleSessionTrait: SpyInstance<[traitName: string, traitValue: SessionTraitValue], Promise<void>>
+  let consoleWarn: SpyInstance<any[], void>
 
-    beforeEach(() => {
-      handleSessionTrait = vi.spyOn(SessionTraitHandler.prototype, 'handle').mockImplementation(asyncNop)
-      consoleWarn = vi.spyOn(global.console, 'warn').mockImplementation(nop)
-    })
-
-    afterEach(() => {
-      handleSessionTrait.mockRestore()
-      consoleWarn.mockRestore()
-    })
-
-    it('delegates session trait to handler', async () => {
-      const collector = await installer().install()
-      await collector.identifySession('connected', true)
-      expect(handleSessionTrait).toHaveBeenCalledWith('connected', true)
-    })
-
-    it('prevents bad format of session trait value', async () => {
-      const collector = await installer().install()
-      await collector.identifySession('connected', { badFormat: true } as unknown as string)
-      expect(consoleWarn).toHaveBeenCalledWith(
-        '[Gravity Data Collector] The following session trait value is not allowed: ',
-        { badFormat: true },
-      )
-      expect(handleSessionTrait).not.toHaveBeenCalled()
-    })
+  beforeEach(() => {
+    handleSessionTrait = vi.spyOn(SessionTraitHandler.prototype, 'handle').mockImplementation(asyncNop)
+    consoleWarn = vi.spyOn(global.console, 'warn').mockImplementation(nop)
   })
-}
 
-contractTest('dry run mode (debug=true)', () => collectorInstaller({ debug: true }))
-contractTest('live mode (debug=false)', () => collectorInstaller({ debug: false, authKey: uuid() }))
+  afterEach(() => {
+    handleSessionTrait.mockRestore()
+    consoleWarn.mockRestore()
+  })
+
+  it('delegates session trait to handler', async () => {
+    const collector = await installer().install()
+    await collector.identifySession('connected', true)
+    expect(handleSessionTrait).toHaveBeenCalledWith('connected', true)
+  })
+
+  it('prevents bad format of session trait value', async () => {
+    const collector = await installer().install()
+    await collector.identifySession('connected', { badFormat: true } as unknown as string)
+    expect(consoleWarn).toHaveBeenCalledWith(
+      '[Gravity Data Collector] The following session trait value is not allowed: ',
+      { badFormat: true },
+    )
+    expect(handleSessionTrait).not.toHaveBeenCalled()
+  })
+})
