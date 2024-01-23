5e8488fe7d80cdc5fc440644264d1d6dae9bd273
Christophe Grandpierre <c.grandpierre@smartesting.com>
2024-01-23 14:07:22 +0100

    catch exception `Uncaught ReferenceError: process is not defined` in method `discoverBuildId`

--- src/user-action/createSessionStartedUserAction.ts
+++ src/user-action/createSessionStartedUserAction.ts
@@ -6,12 +6,16 @@ import gravityDocument from '../utils/gravityDocument'
 import { makeCypressTestContext } from '../utils/makeCypressTestContext'
 
 function discoverBuildId() {
-  return (
-    rejectBlankString((window as { GRAVITY_BUILD_ID?: string }).GRAVITY_BUILD_ID) ??
-    rejectBlankString(process?.env?.GRAVITY_BUILD_ID) ??
-    rejectBlankString(process?.env?.REACT_APP_GRAVITY_BUILD_ID) ??
-    undefined
-  )
+  try {
+    return (
+      rejectBlankString((window as { GRAVITY_BUILD_ID?: string }).GRAVITY_BUILD_ID) ??
+      rejectBlankString(process.env.GRAVITY_BUILD_ID) ??
+      rejectBlankString(process.env.REACT_APP_GRAVITY_BUILD_ID) ??
+      undefined
+    )
+  } catch (e) {
+    return undefined
+  }
 }
 
 function rejectBlankString(value: string | undefined): string | null {
