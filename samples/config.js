971fb3c5248bcc8266ad3bf808c0e8716e0b846e
Christophe Grandpierre <c.grandpierre@smartesting.com>
2024-04-05 11:23:29 +0200

    refactor sample

--- samples/config.js
+++ samples/config.js
@@ -1,6 +1,5 @@
 const config = {
   debug: false,
-  authKey: 'acf27d0a-2b0f-4811-a716-ff4ef24f2e9e', //PROD
-  //authKey: 'fb9bf8be-9235-486c-b029-dfd9ed6262ea', //TEST
+  authKey: '', //key provided by Gravity app (production or test environment)
   gravityServerUrl: 'http://localhost:3000',
 }
