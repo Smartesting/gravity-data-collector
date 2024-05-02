6ae1f2c036498b9f3f42fbc52cc4267c9bd2bee8
Rémi Federspiel <remi.federspiel@smartesting.com>
2024-05-02 10:33:33 +0200

    fix: tsc errors

--- src/test-utils/dummyFactory.ts
+++ src/test-utils/dummyFactory.ts
@@ -1,4 +1,4 @@
-import { DocumentSnapshot } from '../types'
+import { Compressor, DocumentSnapshot } from '../types'
 
 export function createDummy<T>(properties: Partial<T> = {}): T {
   // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
@@ -8,6 +8,7 @@ export function createDummy<T>(properties: Partial<T> = {}): T {
 export function dummyDocumentSnapshot(): DocumentSnapshot {
   return {
     content: '**html**',
+    compressor: Compressor.fflate,
     pathname: '/',
     timestamp: 42,
     viewport: { width: 1200, height: 800 },
