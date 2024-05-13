4d7a0d1f9217d6dc9a034fb577dcb89c0f5c8e19
Vincent Pretre <vincent.pretre@gmail.com>
2024-05-13 13:52:03 +0200

    Remove change that should not have been commited

--- samples/config.js
+++ samples/config.js
@@ -1,5 +1,5 @@
 const config = {
   debug: false,
-  authKey: '8cba94d3-b892-40df-855b-b55623b4ea21', //key provided by Gravity app (production or test environment)
+  authKey: '', //key provided by Gravity app (production or test environment)
   gravityServerUrl: 'http://localhost:3000',
 }
