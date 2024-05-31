2acf75e1526436d156cbc15972253a2660fc7088
Julien Botella <julien.botella@smartesting.com>
2024-05-31 11:18:25 +0200

    authorized sites

--- gravity-chrome-extension/src/contentScript.ts
+++ gravity-chrome-extension/src/contentScript.ts
@@ -2,19 +2,31 @@ import GravityCollector from '../../src'
 import { CollectorOptions } from '../../src/types'
 import { uuid } from '../../src/utils/uuid'
 
-const authorizedSites = [
-    '*****',
-]
+export type MESSAGE_TYPE = {
+    action: string,
+    authKey: string,
+    authorizedSites: string[]
+}
+
+let authorizedSites: string[] = []
 
 const options: Partial<CollectorOptions> = {
     requestInterval: 0
 }
 
 chrome.storage.local.get(
-    ["authenticationKey"],
+    ["authenticationKey", "authorizedSites"],
     function (settings) {
         if (settings.authenticationKey) {
             options.authKey = settings.authenticationKey;
+            if (!options.authKey || options.authKey.length === 0) {
+                options.debug = true
+            } else {
+                options.debug = false
+            }
+        }
+        if (settings.authorizedSites) {
+            authorizedSites = settings.authorizedSites;
         }
         if (options.authKey && options.authKey.length > 0 && authorizedSites.some(authorized => window.location.href.includes(authorized))) {
             GravityCollector.init(options)
@@ -22,7 +34,7 @@ chrome.storage.local.get(
     }
 );
 
-chrome.runtime.onMessage.addListener((message: { action: string, value?: string }) => {
+chrome.runtime.onMessage.addListener((message: MESSAGE_TYPE) => {
     switch (message.action) {
         case 'newSession':
             if (authorizedSites.some(authorized => window.location.href.includes(authorized))) {
@@ -30,8 +42,18 @@ chrome.runtime.onMessage.addListener((message: { action: string, value?: string 
             }
             break
         case 'newAuthenticationKey':
-            if (message.value !== undefined) {
-                options.authKey = message.value
+            if (message.authKey !== undefined) {
+                options.authKey = message.authKey
+                if (!options.authKey || options.authKey.length === 0) {
+                    options.debug = true
+                } else {
+                    options.debug = false
+                }
+            }
+            break;
+        case 'newAuthorizedSites':
+            if (message.authorizedSites !== undefined) {
+                authorizedSites = message.authorizedSites
             }
             break;
         default:
--- gravity-chrome-extension/src/devtools/panel/panel.tsx
+++ gravity-chrome-extension/src/devtools/panel/panel.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import TextField from '@mui/material/TextField';
 import { createRoot } from 'react-dom/client';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import "./panel.css";
 
 const container = document.getElementById("panel")
@@ -11,14 +13,19 @@ if (container) {
 
 function Panel() {
   const [authenticationKey, setAuthenticationKey] = useState<string>('')
+  const [authorizedSites, setAuthorizedSites] = useState<string[]>([])
+  const [newSite, setNewSite] = useState<string>('')
 
   useEffect(() => {
     // @ts-ignore
     chrome.storage.local.get(
-      ["authenticationKey"],
+      ["authenticationKey", "authorizedSites"],
       function (settings) {
         if (settings.authenticationKey) {
           setAuthenticationKey(settings.authenticationKey);
+        }
+        if (settings.authorizedSites) {
+          setAuthorizedSites(settings.authorizedSites);
         }
       }
     );
@@ -31,21 +38,72 @@ function Panel() {
     chrome.tabs.sendMessage(chrome.devtools.inspectedWindow.tabId, { action: 'newAuthenticationKey', value: authenticationKey })
   }, [authenticationKey])
 
+  useEffect(() => {
+    // @ts-ignore
+    chrome.storage.local.set({ authorizedSites });
+    // @ts-ignore
+    chrome.tabs.sendMessage(chrome.devtools.inspectedWindow.tabId, { action: 'newAuthorizedSites', value: authorizedSites })
+  }, [authorizedSites])
+
   return (
     <div className="gravity-data-collector-panel">
-      <button onClick={(e) => {
-        // @ts-ignore
-        chrome.tabs.sendMessage(chrome.devtools.inspectedWindow.tabId, { action: 'newSession' })
-      }}>New session</button>
-      <TextField
-        id="authentication-key"
-        className='gravity-data-collector-authentication-key'
-        label="Authentication Key"
-        value={authenticationKey}
-        type="password"
-        autoComplete="current-auth-key"
-        onBlur={(e) => setAuthenticationKey(e.target.value)}
-      />
-    </div>
+      <div className="gravity-data-collector-panel__left">
+        <button className='primary' onClick={(e) => {
+          // @ts-ignore
+          chrome.tabs.sendMessage(chrome.devtools.inspectedWindow.tabId, { action: 'newSession' })
+          toast.info("New session")
+        }}>New session</button>
+        <h4>Gravity project Settings</h4>
+        <TextField
+          id="authentication-key"
+          className='gravity-data-collector__authentication-key'
+          label="Authentication Key"
+          value={authenticationKey}
+          type="password"
+          autoComplete="current-auth-key"
+          onBlur={(e) => setAuthenticationKey(e.target.value)}
+        />
+        <h4>Authorized Sites</h4>
+        <div className="gravity-data-collector__authorized-sites">
+          {authorizedSites.map((site, index) => {
+            return <div className='gravity-data-collector__authorized-site__line'>
+              <TextField
+                id="authorized-site"
+                key={site}
+                className='gravity-data-collector__authorized-site'
+                label="Authorized site"
+                value={site}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <button className='secondary' onClick={(e) => {
+                const newSites = authorizedSites.slice()
+                newSites.splice(index, 1)
+                setAuthorizedSites(newSites)
+              }}>Remove</button>
+            </div>
+          })}
+          <TextField
+            id="new-authorized-site"
+            className='gravity-data-collector__authorized-site'
+            label="Add authorized site"
+            value={newSite}
+            onChange={(e) => setNewSite(e.target.value)}
+            onBlur={(e) => {
+              if (e.target.value.length > 0) {
+                const newSites = authorizedSites.slice()
+                newSites.push(e.target.value)
+                setAuthorizedSites(newSites)
+                setNewSite('')
+              }
+            }} />
+        </div>
+      </div>
+      <div className="gravity-data-collector-panel__right">
+        <h4>Collector Settings</h4>
+      </div>
+      <ToastContainer />
+    </div >
   );
 }
